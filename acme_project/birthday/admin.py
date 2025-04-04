2b283abf16e12cceef3e920923ef13446c29c5a8
Dmitry Bulhin <bulhin56@yandex.ru>
2025-04-04 16:56:25 +0300

    перед10спринтом

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description'
+    )
+# ...и регистрируем её в админке:
+# admin.site.register(Category)
+admin.site.register(Birthday)
